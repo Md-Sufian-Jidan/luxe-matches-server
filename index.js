34fb2e4b24750828d3ca8ddaea1f611c063bb89a
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-04-23 22:20:40 +0600

    edit a bioData api added

--- index.js
+++ index.js
@@ -45,6 +45,19 @@ async function run() {
             res.send(result);
         });
 
+        //user related api
+        app.put('/bio-data-edit/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const bioData = req.body;
+            const updateBioData = {
+                $set: { bioData }
+            };
+            console.log(updateBioData);
+            const result = await userCollection.updateOne(query, updateBioData);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
